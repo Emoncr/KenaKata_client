207dd5a87a7a8873013cdb1b520b7ff4b2a97e52
Emoncr <biplobemon 75945@gmail.com>
2024-04-03 03:22:16 +0600

    setup- project setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,35 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        quicksand: ["var(--font-quicksand)"],
+        lato: ["var(--font-lato)"],
+      },
+      colors: {
+        skin: {
+          brand: "var(--color-brand)",
+          brandDark: "var(--color-brand-dark)",
+          brand2: "var(--color-brand-2)",
+          primary: "var(--color-primary)",
+          secondary: "var(--color-secondary)",
+          warning: "var(--color-warning)",
+          danger: "var(--color-danger)",
+          success: "var(--color-success)",
+          info: "var(--color-info)",
+        },
+      },
+      textColor: {
+        skin: {
+          text: "var(--color-text)",
+          heading: "var(--color-heading)",
+          grey1: "var(--color-grey-1)",
+          grey2: "var(--color-grey-2)",
+          grey4: "var(--color-grey-4)",
+          grey9: "var(--color-grey-9)",
+          muted: "var(--color-muted)",
+          textBody: "var(--color-text-body)",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
