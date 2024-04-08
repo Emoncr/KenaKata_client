0c8ea62904b5eb4c19130646e61d301bcfb6ea6b
Emoncr <biplobemon 75945@gmail.com>
2024-04-08 06:53:52 +0600

    create - fix - fix comment component

--- src/components/Pd page components/ProductDetails.jsx
+++ src/components/Pd page components/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Star, Plus, Minus, ShoppingCart, Heart, GitCompareArrows } from "lucide-react"
 import Link from "next/link"
+import CommentForm from '../FormComponents/CommentForm'
 
 
 const ProductDetails = () => {
@@ -122,33 +123,12 @@ const ProductDetails = () => {
                         <p className='text-skin-text font-lato font-medium text-base mt-3'>
                             Your email address will not be published. Required fields are marked *
                         </p>
-                        <p className='text-skin-text font-lato font-medium text-base mt-3'>
-                            Your rating *
-                        </p>
-                        <div>
-                            <div className='flex items-center justify-start gap-0 mt-1'>
-                                <Star size={20} />
-                                <Star size={20} />
-                                <Star size={20} />
-                                <Star size={20} />
-                                <Star size={20} />
-                            </div>
-                            <p className='text-skin-text font-lato font-medium text-base mt-5'>
-                                Your review *
-                            </p>
-                            <form>
-                                <textarea className='border-border_2 border border-solid p-3 mt-1 focus:outline-skin-brand w-full rounded-md font-lato font-normal' name="comment" cols="30" rows="10">
-                                </textarea>
-                                <button type='submit' className='px-6 py-2.5 bg-skin-brand hover:bg-skin-brandDark text-white rounded-lg font-quicksand font-semibold '>
-                                    Add Comment
-                                </button>
-                            </form>
-                        </div>
+
+                        <CommentForm />
                     </div>
                 </div>
-
             </div>
-        </div>
+        </div >
     )
 }
 
